a57e0be15e025df2f010dfde6000d53f13a882bd
souloukos <souloukos@itsaur.com>
2023-06-20 09:43:13 +0300

    unused classes deletion

--- src/main/java/com/itsaur/internship/PostgresUsersStore.java
+++ src/main/java/com/itsaur/internship/PostgresUsersStore.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
-public class PostgresUsersStore implements UsersStore, ProductsStore {
+public class PostgresUsersStore implements UsersStore {
     Vertx vertx = Vertx.vertx();
     private final PgConnectOptions connectOptions;
     private final PoolOptions poolOptions;
