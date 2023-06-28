eb38525f3d28f44862f26de77524e0e6ba2cda97
souloukos <souloukos@itsaur.com>
2023-06-28 11:02:39 +0300

    add CartsStore to Git

---
+++ src/main/java/com/itsaur/internship/CartsStore.java
@@ -0,0 +1,23 @@
+package com.itsaur.internship;
+
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
+
+import java.util.UUID;
+
+public interface CartsStore {
+
+    Future<Void> checkQuantity(UUID id, int quantity);
+
+    Future<Void> addToCart(UUID uid, UUID pid, int quantity);
+
+    Future<Boolean> findCart(UUID id);
+
+    Future<Boolean> findItem(UUID uid, UUID pid);
+
+    Future<Void> removeFromCart(UUID uid, UUID pid, int quantity);
+
+    Future<JsonArray> cart(UUID uid);
+
+    Future<Void> buy(UUID uid);
+}
