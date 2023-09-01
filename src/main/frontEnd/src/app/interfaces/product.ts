3518f74b44c1627b64c18610f15cb8c1e7e115e9
souloukos <souloukos@itsaur.com>
2023-09-01 16:05:08 +0300

    modified fields

--- src/main/frontEnd/src/app/interfaces/product.ts
+++ src/main/frontEnd/src/app/interfaces/product.ts
@@ -1,7 +1,7 @@
 export interface Product {
   pid: string | null | undefined;
   name: string | null | undefined;
-  image: string | null | undefined;
+  // image: string | null | undefined;
   description: string | null | undefined;
   price: number | null | undefined;
   quantity: number | null | undefined;
