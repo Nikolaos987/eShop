896d367d0f1418fd9b82179ec3caef5cf2cbc2ec
souloukos <souloukos@itsaur.com>
2023-09-05 16:37:11 +0300

    fixed navigation bar

--- src/main/frontEnd/src/app/app.component.ts
+++ src/main/frontEnd/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
@@ -7,6 +7,23 @@ import { Observable } from "rxjs";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('topnav') menuElement: ElementRef | undefined;
+  menuPosition: any;
+  sticky: boolean = false;
+
+  ngAfterViewInit() {
+    this.menuPosition = this.menuElement?.nativeElement.offsetTop
+  }
+
+  @HostListener('window:scroll', ['event'])
+  handleScroll() {
+    const windowScroll = window.scrollY;
+    if (windowScroll >= 80) {
+      this.sticky = true;
+    } else {
+      this.sticky = false;
+    }
+  }
 
 }
